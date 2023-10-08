2845d08aa8b6d7b974c74d5abef207f69e367382
Tushar Thokdar <85017697+Tushar365@users.noreply.github.com>
2023-10-08 12:07:44 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPER_EDOT="-e ."
+
+def get_requrements(file_path:str)->list[str]:
+    # This function will return the list of requrements
+    requrements=[]
+    with open(file_path) as file_obj:
+        requrements=file_obj.readlines()
+        requrements=[req.replace("\n","") for req in requrements]
+        if HYPER_EDOT in requrements:
+            requrements.remove(HYPER_EDOT)
+
+    return requrements
+setup(
+    
+    
+name='ML-project-ALL',
+version='0.0.1',
+author='Tushar Thokdar',
+author_email='tusharthokdarp@gmail.com',
+packages=find_packages(),
+install_requrements=get_requrements("requrements.txt")
+  
+    
+)
