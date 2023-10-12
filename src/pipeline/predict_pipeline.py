239d8d4c444f4046013da74f7b3e05363fa745d4
Tushar Thokdar <85017697+Tushar365@users.noreply.github.com>
2023-10-12 16:29:11 +0530

    deploy

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,29 +1,7 @@
 import sys
-import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
-
-
-class PredictPipeline:
-    def __init__(self):
-        pass
-
-    def predict(self,features):
-        try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
-            return preds
-        
-        except Exception as e:
-            raise CustomException(e,sys)
-
 
 
 class PredictPipeline:
@@ -76,7 +54,7 @@ class CustomData:
             custom_data_input_dict = {
                 "gender": [self.gender],
                 "race_ethnicity": [self.race_ethnicity],
-                "parental level of education": [self.parental_level_of_education],
+                "parental_level_of_education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
                 "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
